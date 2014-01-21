bcf32f1a47124b0c3563aafc1ac91be33b59306e
borisvidolov <borisv@microsoft.com>
2014-01-21 12:17:19 -0800

    Finding the web view

--- Samples/MyTestiOSApp/MyTestiOSAppTests/MyTestiOSAppTests.m
+++ Samples/MyTestiOSApp/MyTestiOSAppTests/MyTestiOSAppTests.m
@@ -9,6 +9,7 @@
 #import <XCTest/XCTest.h>
 #import <ADALiOS/ADAuthenticationContext.h>
 #import "BVTestAppDelegate.h"
+#import <ADAliOS/ADAuthenticationSettings.h>
 
 @interface MyTestiOSAppTests : XCTestCase
 
@@ -33,44 +34,71 @@
     //TBD
 }
 
+-(UIWebView*) findWebView: (UIWindow*) parent
+{
+    NSArray* windows = (parent) ? [parent subviews] : [[UIApplication sharedApplication] windows];
+    for(UIWindow* window in windows)
+    {
+        if ([window isKindOfClass:[UIWebView class]])
+        {
+            return (UIWebView*)window;
+        }
+        else
+        {
+            UIWebView* result = [self findWebView:window];
+            if (result)
+                return result;
+        }
+    }
+    return nil;
+}
+
 - (void)testExample2
 {
-//    NSString* authority = @"https://login.windows.net/msopentechbv.onmicrosoft.com";//OmerCan: params.authority
-//    NSString* clientId = @"c3c7f5e5-7153-44d4-90e6-329686d48d76";//OmerCan: @"c4acbce5-b2ed-4dc5-a1b9-c95af96c0277"
-//    NSString* resourceString = @"http://localhost/TodoListService";
-//    NSString* redirectUri = @"http://todolistclient/";//OmerCan: @"https://omercantest.onmicrosoft.adal/hello"
-////    [weakSelf setStatus:[NSString stringWithFormat:@"Authority: %@", params.authority]];
-//    ADAuthenticationError* error;
-//    ADAuthenticationContext* context = [ADAuthenticationContext authenticationContextWithAuthority:authority error:&error];
-//    XCTAssertNotNil(context);
-//    [context.tokenCacheStore removeAll];//Clear the cache12
-//    __block ADAuthenticationResult* localResult;
-//    
-//    [context acquireTokenWithResource:resourceString clientId:clientId
-//                          redirectUri:[NSURL URLWithString:redirectUri]
-//                               userId:@"boris@msopentechbv.onmicrosoft.com"
-//                      completionBlock:^(ADAuthenticationResult *result)
-//    {
-//        localResult = result;
-//    }];
-//    NSDate* firstTimeOut = [NSDate dateWithTimeIntervalSinceNow:3];//Waits for 10 seconds.
-//    while (!localResult && [[NSDate dateWithTimeIntervalSinceNow:0] compare:firstTimeOut] != NSOrderedDescending)
-//    {
-//        [[NSRunLoop mainRunLoop] runMode:NSDefaultRunLoopMode beforeDate:firstTimeOut];
-//    }
-//    UIWindow* window = [[UIApplication sharedApplication] keyWindow];
-//    [window sendEvent:<#(UIEvent *)#>]
-//    XCTAssertNotNil(window);
-//    NSDate* timeOut = [NSDate dateWithTimeIntervalSinceNow:10];//Waits for 10 seconds.
-//    while (!localResult && [[NSDate dateWithTimeIntervalSinceNow:0] compare:timeOut] != NSOrderedDescending)
-//    {
-//        [[NSRunLoop mainRunLoop] runMode:NSDefaultRunLoopMode beforeDate:timeOut];
-//    }
-//    if (!localResult)
-//    {
-//        XCTFail("Time out.");
-//        return;
-//    }
+    //Clear all cookies:
+    NSHTTPCookieStorage* cookiesStorage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
+    NSMutableArray* allCookies = [NSMutableArray arrayWithArray:cookiesStorage.cookies];
+    for(NSHTTPCookie* cookie in allCookies)
+    {
+        [cookiesStorage deleteCookie:cookie];
+    }
+    [ADAuthenticationSettings sharedInstance].singleSignOn = NO;
+    NSString* authority = @"https://login.windows.net/msopentechbv.onmicrosoft.com";//OmerCan: params.authority
+    NSString* clientId = @"c3c7f5e5-7153-44d4-90e6-329686d48d76";//OmerCan: @"c4acbce5-b2ed-4dc5-a1b9-c95af96c0277"
+    NSString* resourceString = @"http://localhost/TodoListService";
+    NSString* redirectUri = @"http://todolistclient/";//OmerCan: @"https://omercantest.onmicrosoft.adal/hello"
+//    [weakSelf setStatus:[NSString stringWithFormat:@"Authority: %@", params.authority]];
+    ADAuthenticationError* error;
+    ADAuthenticationContext* context = [ADAuthenticationContext authenticationContextWithAuthority:authority error:&error];
+    XCTAssertNotNil(context);
+    [context.tokenCacheStore removeAll];//Clear the cache12
+    __block ADAuthenticationResult* localResult;
+    
+    [context acquireTokenWithResource:resourceString clientId:clientId
+                          redirectUri:[NSURL URLWithString:redirectUri]
+                               userId:@"boris@msopentechbv.onmicrosoft.com"
+                      completionBlock:^(ADAuthenticationResult *result)
+    {
+        localResult = result;
+    }];
+    NSDate* firstTimeOut = [NSDate dateWithTimeIntervalSinceNow:3];//Waits for 10 seconds.
+    while (!localResult && [[NSDate dateWithTimeIntervalSinceNow:0] compare:firstTimeOut] != NSOrderedDescending)
+    {
+        [[NSRunLoop mainRunLoop] runMode:NSDefaultRunLoopMode beforeDate:firstTimeOut];
+    }
+    UIWebView* webView = [self findWebView:nil];
+    
+        XCTAssertNotNil(webView);
+    NSDate* timeOut = [NSDate dateWithTimeIntervalSinceNow:10];//Waits for 10 seconds.
+    while (!localResult && [[NSDate dateWithTimeIntervalSinceNow:0] compare:timeOut] != NSOrderedDescending)
+    {
+        [[NSRunLoop mainRunLoop] runMode:NSDefaultRunLoopMode beforeDate:timeOut];
+    }
+    if (!localResult)
+    {
+        XCTFail("Time out.");
+        return;
+    }
 }
 
 
