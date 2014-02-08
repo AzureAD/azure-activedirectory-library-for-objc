8af317c15f81f08999ad738a734eda8aba95cd6a
borisvidolov <borisv@microsoft.com>
2014-02-07 17:21:32 -0800

    More tests to improve code coverage numbers

--- ADALiOS/ADALiOSTests/ADAuthenticationContextTests.m
+++ ADALiOS/ADALiOSTests/ADAuthenticationContextTests.m
@@ -1012,8 +1012,8 @@ const int sAsyncContextTimeout = 10;
 }
 
 //Tests the shorter overload of acquireTokenByRefreshToken
-//as the method ultimately calls the other overload, which is also used by acquireToken,
-//this test is not very deep.
+//as the method ultimately calls the other overload, which is also used by acquireToken.
+//As such, the test is not very deep.
 -(void) testAcquireTokenByRefreshTokenSimple_Negative
 {
     //There is no resource for this call:
@@ -1138,6 +1138,33 @@ const int sAsyncContextTimeout = 10;
     [self asyncAcquireTokenByRefreshToken:@"refreshToken"];
     ADAssertLongEquals(AD_FAILED, mResult.status);
     [self validateForInvalidArgument:@"clientId" error:mResult.error];
+}
+
+//Hits a the cloud with either a bad server or a bad refresh token:
+-(void) testRefreshingTokenWithServerErrors
+{
+    //Authority cannot be validated or reached:
+    mContext = [ADAuthenticationContext authenticationContextWithAuthority:@"https://example.com/common" error:nil];
+    XCTAssertNotNil(mContext);
+
+    [self asyncAcquireTokenByRefreshToken:@"doesn't matter"];
+    ADAssertLongEquals(AD_FAILED, mResult.status);
+    ADAssertLongEquals(AD_ERROR_AUTHORITY_VALIDATION, mResult.error.code);
+    
+    mContext.validateAuthority = NO;
+    [self asyncAcquireTokenByRefreshToken:@"doesn't matter"];
+    ADAssertLongEquals(AD_FAILED, mResult.status);
+    ADAssertStringEquals(mResult.error.domain, NSURLErrorDomain);
+    
+    //Valid authority, but invalid refresh token:
+    mContext = [ADAuthenticationContext authenticationContextWithAuthority:mAuthority error:nil];
+    XCTAssertNotNil(mContext);
+    
+    [self asyncAcquireTokenByRefreshToken:@"invalid_refresh_token"];
+    ADAssertLongEquals(AD_FAILED, mResult.status);
+    ADAssertLongEquals(AD_ERROR_INVALID_REFRESH_TOKEN, mResult.error.code);
+    ADAssertStringEquals(mResult.error.protocolCode, @"invalid_grant");
+    XCTAssertTrue([mResult.error.errorDetails.lowercaseString containsString:@"refresh token"]);
 }
 
 @end
