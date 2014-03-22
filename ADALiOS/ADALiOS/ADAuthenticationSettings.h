a22b9063105ff033afe02e37b1e3aa0de5990fb0
borisvidolov <borisv@microsoft.com>
2014-03-21 17:43:35 -0700

    More applicable updates

--- ADALiOS/ADALiOS/ADAuthenticationSettings.h
+++ ADALiOS/ADALiOS/ADAuthenticationSettings.h
@@ -15,6 +15,7 @@
 //
 // See the Apache License, Version 2.0 for the specific language
 // governing permissions and limitations under the License.
+#import <Foundation/Foundation.h>
 
 @protocol ADTokenCacheStoring;
 /*!
