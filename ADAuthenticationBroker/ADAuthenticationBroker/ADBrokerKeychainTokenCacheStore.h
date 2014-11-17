1fced4dd170ee2688dd00f984ce476ead3df886f
Kanishk Panwar <kpanwar@microsoft.com>
2014-11-17 13:55:14 -0800

    Broker work
    Broker work

---
+++ ADAuthenticationBroker/ADAuthenticationBroker/ADBrokerKeychainTokenCacheStore.h
@@ -0,0 +1,35 @@
+// Copyright © Microsoft Open Technologies, Inc.
+//
+// All Rights Reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+// ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A
+// PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+//
+// See the Apache License, Version 2.0 for the specific language
+// governing permissions and limitations under the License.
+
+#import <ADALiOS/ADTokenCacheStoring.h>
+
+@interface ADBrokerKeychainTokenCacheStore : NSObject<ADTokenCacheStoring>
+
+/*! Initializes the token cache store.
+ @param: sourceApplication: Required. If the application needs to share the cached tokens
+ with other applications from the same vendor, the app will need to specify the
+ shared group here and add the necessary entitlements to the application.
+ See Apple's keychain services documentation for details. */
+-(id) initWithSourceApp: (NSString*) sourceApplication;
+
+/* The shared keychain group, where the ADAL library will keep the tokens.
+ May be nil. The cache items from the previous keychain group are not transferred
+ automatically. */
+@property (getter = getSourceApplication, setter = setSourceApplication:) NSString* sourceApplication;
+
+@end
