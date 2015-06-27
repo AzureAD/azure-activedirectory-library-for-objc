9a47b40871010d5470a6dc6589c8dc7d222abfb1
Ryan Pangrle <rpangrle@microsoft.com>
2015-06-26 19:54:04 -0700

    Add ADAuthenticationRequest and move some of the ADAuthenticationContext categories to ADAuthenticationRequest

---
+++ ADALiOS/ADALiOS/ADAuthenticationRequest.h
@@ -0,0 +1,13 @@
+//
+//  ADAuthenticationRequest.h
+//  ADALiOS
+//
+//  Created by Ryan Pangrle on 6/26/15.
+//  Copyright (c) 2015 MS Open Tech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ADAuthenticationRequest : NSObject
+
+@end
---
+++ ADALiOS/ADALiOS/ADAuthenticationRequest.m
@@ -0,0 +1,13 @@
+//
+//  ADAuthenticationRequest.m
+//  ADALiOS
+//
+//  Created by Ryan Pangrle on 6/26/15.
+//  Copyright (c) 2015 MS Open Tech. All rights reserved.
+//
+
+#import "ADAuthenticationRequest.h"
+
+@implementation ADAuthenticationRequest
+
+@end
