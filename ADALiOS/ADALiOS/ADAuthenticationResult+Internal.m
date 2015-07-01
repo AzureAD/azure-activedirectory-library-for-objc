792752fa7def01d7b27ce3ab222c14edd91270a8
Ryan Pangrle <rpangrle@microsoft.com>
2015-07-01 13:34:29 -0700

    Copy & fix code in resultFromBrokerResponse: that didn't make it into OSXUniversal

--- ADALiOS/ADALiOS/ADAuthenticationResult+Internal.m
+++ ADALiOS/ADALiOS/ADAuthenticationResult+Internal.m
@@ -17,6 +17,7 @@
 // governing permissions and limitations under the License.
 
 
+#import "ADAL.h"
 #import "ADAuthenticationResult.h"
 #import "ADAuthenticationResult+Internal.h"
 #import "ADTokenCacheStoreItem.h"
@@ -102,40 +103,95 @@ multiResourceRefreshToken: (BOOL) multiResourceRefreshToken
     return SAFE_ARC_AUTORELEASE(result);
 }
 
-+(ADAuthenticationResult*) resultFromBrokerResponse: (NSDictionary*) response
+ +(ADAuthenticationResult*)resultFromBrokerResponse:(NSDictionary*)response
 {
-    ADAuthenticationError* error = nil;
+    ADAuthenticationResult* result = nil;
     ADTokenCacheStoreItem* item = nil;
-    if([response valueForKey:OAUTH2_ERROR_DESCRIPTION])
+    
+    if(!response || [response valueForKey:OAUTH2_ERROR_DESCRIPTION])
     {
-        error = [ADAuthenticationError errorFromNSError:[NSError errorWithDomain:ADBrokerResponseErrorDomain code:0 userInfo:nil] errorDetails:[response valueForKey:OAUTH2_ERROR_DESCRIPTION]];
+        ADAuthenticationError* error = nil;
+        NSString* errorDetails = nil;
+        NSInteger errorCode = 0;
+        if (response)
+        {
+            errorDetails = [response valueForKey:OAUTH2_ERROR_DESCRIPTION];
+            errorCode = [[response valueForKey:@"error_code"] integerValue];
+            
+            if (!errorDetails)
+            {
+                errorDetails = @"Broker did not provide any details";
+            }
+        }
+        else
+        {
+            errorDetails = @"No broker response received.";
+        }
+        
+        error = [ADAuthenticationError errorFromNSError:[NSError errorWithDomain:ADBrokerResponseErrorDomain code:errorCode userInfo:nil] errorDetails:errorDetails];
+        
         return [ADAuthenticationResult resultFromError:error];
     }
-
+    
+    ADUserInformation* info = nil;
+    if([response valueForKey:OAUTH2_ID_TOKEN])
+    {
+        ADAuthenticationError* error = nil;
+        info = [ADUserInformation userInformationWithIdToken:[response valueForKey:OAUTH2_ID_TOKEN] error:&error];
+        if (error)
+        {
+            return [ADAuthenticationResult resultFromError:error];
+        }
+    }
+    
     item = [ADTokenCacheStoreItem new];
+    item.userInformation = info;
     item.authority =  [response valueForKey:OAUTH2_AUTHORITY];
     item.resource = [response valueForKey:OAUTH2_RESOURCE];
     item.clientId = [response valueForKey:OAUTH2_CLIENT_ID];
     item.accessToken = [response valueForKey:OAUTH2_ACCESS_TOKEN];
     item.refreshToken = [response valueForKey:OAUTH2_REFRESH_TOKEN];
-    if([response valueForKey:OAUTH2_ID_TOKEN])
+    
+    result.tokenCacheStoreItem.accessTokenType = @"Bearer";
+    // Token response
+    id expires_in = [response objectForKey:@"expires_on"];
+    NSDate *expires    = nil;
+    
+    if ( expires_in != nil )
     {
-        ADUserInformation* info = [ADUserInformation userInformationWithIdToken:[response valueForKey:OAUTH2_ID_TOKEN] error:&error];
-        if(!error)
+        if ( [expires_in isKindOfClass:[NSString class]] )
         {
-            item.userInformation = info;
+            NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+            SAFE_ARC_AUTORELEASE(formatter);
+            
+            expires = [NSDate dateWithTimeIntervalSinceNow:[formatter numberFromString:expires_in].longValue];
+        }
+        else if ( [expires_in isKindOfClass:[NSNumber class]] )
+        {
+            expires = [NSDate dateWithTimeIntervalSinceNow:((NSNumber *)expires_in).longValue];
         }
         else
         {
-            SAFE_ARC_RELEASE(item);
-            return [ADAuthenticationResult resultFromError:error];
+            AD_LOG_WARN_F(@"Unparsable time", @"The response value for the access token expiration cannot be parsed: %@", expires);
+            // Unparseable, use default value
+            expires = [NSDate dateWithTimeIntervalSinceNow:3600.0];//1 hour
         }
     }
+    else
+    {
+        AD_LOG_WARN(@"Missing expiration time.", @"The server did not return the expiration time for the access token.");
+        expires = [NSDate dateWithTimeIntervalSinceNow:3600.0];//Assume 1hr expiration
+    }
     
-    ADAuthenticationResult* result = [[ADAuthenticationResult alloc] initWithItem:item multiResourceRefreshToken:NO];
-    SAFE_ARC_RELEASE(item);
+    result.tokenCacheStoreItem.expiresOn = expires;
+    
+    
+    BOOL isMRRT = item.resource && item.refreshToken;
 
-    return SAFE_ARC_AUTORELEASE(result);
+    result = [[ADAuthenticationResult alloc] initWithItem:item multiResourceRefreshToken:isMRRT];
+    SAFE_ARC_AUTORELEASE(result);
+    SAFE_ARC_RELEASE(item);
+    return result;
 }
 
 @end
