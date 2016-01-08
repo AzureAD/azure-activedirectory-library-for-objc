432961a070d2ce860e7aec764f411522f68173f1
Yong Zeng <jasoncoolmax@Yongs-Mac-mini.local>
2016-01-08 11:07:00 -0800

    Delete unused functions in previous change.

--- ADALiOS/ADALiOS/ADAuthenticationContext+Internal.h
+++ ADALiOS/ADALiOS/ADAuthenticationContext+Internal.h
@@ -62,8 +62,6 @@ extern NSString* const ADRedirectUriInvalidError;
 
 + (BOOL)isFinalResult:(ADAuthenticationResult*)result;
 
-+ (BOOL)isServerError:(ADAuthenticationResult*)result;
-
 + (NSString*)getPromptParameter:(ADPromptBehavior)prompt;
 
 + (BOOL)isForcedAuthorization:(ADPromptBehavior)prompt;
