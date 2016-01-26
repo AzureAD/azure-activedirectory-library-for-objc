4039f8742c6df1c4fda0a69a0e772ed622e26535
Ryan Pangrle <rpangrle@microsoft.com>
2016-01-26 14:15:18 -0800

    Shuffle around files. Remove "iOS" from the name of the project and various headers. Remove unused files.

---
+++ ADAL/src/ADAuthenticationResult+Internal.m
@@ -0,0 +1,198 @@
+// Copyright © Microsoft Open Technologies, Inc.
+//
+// All Rights Reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+// ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A
+// PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+//
+// See the Apache License, Version 2.0 for the specific language
+// governing permissions and limitations under the License.
+
+
+#import "ADAL.h"
+#import "ADAuthenticationResult.h"
+#import "ADAuthenticationResult+Internal.h"
+#import "ADTokenCacheItem+Internal.h"
+#import "ADOAuth2Constants.h"
+#import "ADUserInformation.h"
+
+@implementation ADAuthenticationResult (Internal)
+
+-(id) initWithCancellation: (NSUUID*) correlationId
+{
+    ADAuthenticationError* error = [ADAuthenticationError errorFromCancellation];
+    
+    return [self initWithError:error status:AD_USER_CANCELLED correlationId:correlationId];
+}
+
+-(id) initWithItem: (ADTokenCacheItem*) item
+multiResourceRefreshToken: (BOOL) multiResourceRefreshToken
+     correlationId: (NSUUID*) correlationId
+{
+    self = [super init];
+    if (self)
+    {
+        _status = AD_SUCCEEDED;
+        _tokenCacheItem = item;
+        _multiResourceRefreshToken = multiResourceRefreshToken;
+        _correlationId = correlationId;
+    }
+    return self;
+}
+
+-(id) initWithError: (ADAuthenticationError*)error
+             status: (ADAuthenticationResultStatus) status
+      correlationId: (NSUUID*) correlationId
+{
+    THROW_ON_NIL_ARGUMENT(error);
+    
+    self = [super init];
+    if (self)
+    {
+        _status = status;
+        _error = error;
+        _correlationId = correlationId;
+    }
+    return self;
+}
+
++ (ADAuthenticationResult*)resultFromTokenCacheItem:(ADTokenCacheItem *)item
+                               multiResourceRefreshToken:(BOOL)multiResourceRefreshToken
+                                           correlationId:(NSUUID *)correlationId
+{
+    if (!item)
+    {
+        ADAuthenticationError* error = [ADAuthenticationError unexpectedInternalError:@"ADAuthenticationResult created from nil token item."];
+        return [ADAuthenticationResult resultFromError:error];
+    }
+    
+    return [[ADAuthenticationResult alloc] initWithItem:item multiResourceRefreshToken:multiResourceRefreshToken correlationId:correlationId];
+}
+
++(ADAuthenticationResult*) resultFromError: (ADAuthenticationError*) error
+{
+    return [self resultFromError:error correlationId:nil];
+}
+
++(ADAuthenticationResult*) resultFromError: (ADAuthenticationError*) error
+                             correlationId: (NSUUID*) correlationId
+{
+    ADAuthenticationResult* result = [ADAuthenticationResult alloc];
+    return [result initWithError:error status:AD_FAILED correlationId:correlationId];
+}
+
++ (ADAuthenticationResult*)resultFromParameterError:(NSString *)details
+{
+    return [self resultFromParameterError:details correlationId:nil];
+}
+
++ (ADAuthenticationResult*)resultFromParameterError:(NSString *)details
+                                      correlationId: (NSUUID*) correlationId
+{
+    return [[ADAuthenticationResult alloc] initWithError:[ADAuthenticationError invalidArgumentError:details] status:AD_FAILED correlationId:correlationId];
+}
+
++(ADAuthenticationResult*) resultFromCancellation
+{
+    return [self resultFromCancellation:nil];
+}
+
++(ADAuthenticationResult*) resultFromCancellation: (NSUUID*) correlationId
+{
+    ADAuthenticationResult* result = [ADAuthenticationResult alloc];
+    return [result initWithCancellation:correlationId];
+}
+
++ (ADAuthenticationResult*)resultForNoBrokerResponse
+{
+    NSError* nsError = [NSError errorWithDomain:ADBrokerResponseErrorDomain
+                                           code:AD_ERROR_BROKER_UNKNOWN
+                                       userInfo:nil];
+    ADAuthenticationError* error = [ADAuthenticationError errorFromNSError:nsError
+                                                              errorDetails: @"No broker response received."];
+    return [ADAuthenticationResult resultFromError:error correlationId:nil];
+}
+
++ (ADAuthenticationResult*)resultForBrokerErrorResponse:(NSDictionary*)response
+{
+	NSUUID* correlationId = [response valueForKey:OAUTH2_CORRELATION_ID_RESPONSE] ?
+                            [[NSUUID alloc] initWithUUIDString:[response valueForKey:OAUTH2_CORRELATION_ID_RESPONSE]]
+                            : nil;
+    
+    // Otherwise parse out the error condition
+    ADAuthenticationError* error = nil;
+    
+    NSString* errorDetails = [response valueForKey:OAUTH2_ERROR_DESCRIPTION];
+    if (!errorDetails)
+    {
+        errorDetails = @"Broker did not provide any details";
+    }
+        
+    NSString* strErrorCode = [response valueForKey:@"error_code"];
+    NSInteger errorCode = AD_ERROR_BROKER_UNKNOWN;
+    if (strErrorCode && ![strErrorCode isEqualToString:@"0"])
+    {
+        errorCode = [strErrorCode integerValue];
+    }
+    
+    NSString* protocolCode = [response valueForKey:@"protocol_code"];
+    if (!protocolCode)
+    {
+        // Older brokers used to send the protocol code as "code" and the error code not at all
+        protocolCode = [response valueForKey:@"code"];
+    }
+    
+    if (![NSString adIsStringNilOrBlank:protocolCode])
+    {
+       
+        error = [ADAuthenticationError errorFromAuthenticationError:errorCode
+                                                       protocolCode:protocolCode
+                                                       errorDetails:errorDetails];
+    }
+    else
+    {
+        NSError* nsError = [NSError errorWithDomain:ADBrokerResponseErrorDomain
+                                               code:errorCode
+                                           userInfo:nil];
+        error = [ADAuthenticationError errorFromNSError:nsError errorDetails:errorDetails];
+    }
+    
+    return [ADAuthenticationResult resultFromError:error correlationId:correlationId];
+
+}
+
++ (ADAuthenticationResult *)resultFromBrokerResponse:(NSDictionary *)response
+{
+    if (!response)
+    {
+        return [self resultForNoBrokerResponse];
+    }
+    
+    if ([response valueForKey:OAUTH2_ERROR_DESCRIPTION])
+    {
+        return [self resultForBrokerErrorResponse:response];
+    }
+    
+    NSUUID* correlationId =  nil;
+    NSString* correlationIdStr = [response valueForKey:OAUTH2_CORRELATION_ID_RESPONSE];
+    if (correlationIdStr)
+    {
+        correlationId = [[NSUUID alloc] initWithUUIDString:correlationIdStr];
+    }
+
+    ADTokenCacheItem* item = [ADTokenCacheItem new];
+    [item setAccessTokenType:@"Bearer"];
+    BOOL isMRRT = [item fillItemWithResponse:response];
+    return [[ADAuthenticationResult alloc] initWithItem:item multiResourceRefreshToken:isMRRT correlationId:correlationId];
+    
+}
+
+@end
---
+++ ADAL/src/urlprotocol/ADURLProtocol.m
@@ -0,0 +1,216 @@
+// Copyright © Microsoft Open Technologies, Inc.
+//
+// All Rights Reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+// ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A
+// PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+//
+// See the Apache License, Version 2.0 for the specific language
+// governing permissions and limitations under the License.
+
+#import "ADAL.h"
+#import "ADURLProtocol.h"
+#import "ADLogger.h"
+#import "ADNTLMHandler.h"
+#import "ADCustomHeaderHandler.h"
+
+static NSString* const sLog = @"HTTP Protocol";
+static NSMutableDictionary* s_handlers = nil;
+
+
+@implementation ADURLProtocol
+{
+    NSURLConnection *_connection;
+}
+
++ (void)registerHandler:(id)handler
+             authMethod:(NSString*)authMethod
+{
+    if (!handler || !authMethod)
+    {
+        return;
+    }
+    
+    authMethod = [authMethod lowercaseString];
+    
+    @synchronized(self)
+    {
+        static dispatch_once_t once;
+        dispatch_once(&once, ^{
+            s_handlers = [NSMutableDictionary new];
+        });
+        
+        [s_handlers setValue:handler forKey:authMethod];
+    }
+}
+
+
++ (BOOL)registerProtocol
+{
+    return [NSURLProtocol registerClass:self];
+}
+
++ (void)unregisterProtocol
+{
+    [NSURLProtocol unregisterClass:self];
+    
+    @synchronized(self)
+    {
+        for (NSString* key in s_handlers)
+        {
+            Class<ADAuthMethodHandler> handler = [s_handlers objectForKey:key];
+            [handler resetHandler];
+        }
+    }
+}
+
++ (BOOL)canInitWithRequest:(NSURLRequest *)request
+{
+    //TODO: Experiment with filtering of the URL to ensure that this class intercepts only
+    //ADAL initiated webview traffic, INCLUDING redirects. This may have issues, if requests are
+    //made from javascript code, instead of full page redirection. As such, I am intercepting
+    //all traffic while authorization webview session is displayed for now.
+    if ( [[request.URL.scheme lowercaseString] isEqualToString:@"https"] )
+    {
+        //This class needs to handle only TLS. The check below is needed to avoid infinite recursion between starting and checking
+        //for initialization
+        if ( [NSURLProtocol propertyForKey:@"ADURLProtocol" inRequest:request] == nil )
+        {
+            AD_LOG_VERBOSE_F(sLog, nil, @"Requested handling of URL host: %@", [request.URL host]);
+
+            return YES;
+        }
+    }
+    
+    AD_LOG_VERBOSE_F(sLog, nil, @"Ignoring handling of URL host: %@", [request.URL host]);
+    
+    return NO;
+}
+
++ (NSURLRequest *)canonicalRequestForRequest:(NSURLRequest *)request
+{
+    AD_LOG_VERBOSE_F(sLog, nil, @"canonicalRequestForRequest host: %@", [request.URL host] );
+    
+    return request;
+}
+
+- (void)startLoading
+{
+    if (!self.request)
+    {
+        AD_LOG_WARN(sLog, nil, @"startLoading called without specifying the request.");
+        return;
+    }
+    
+    [self startLoading:self.request.URL];
+}
+
+- (void)startLoading:(NSURL*)url
+{
+    AD_LOG_VERBOSE_F(sLog, nil, @"startLoading host: %@", [url host] );
+    NSMutableURLRequest *mutableRequest = [self.request mutableCopy];
+    [mutableRequest setURL:url];
+    [NSURLProtocol setProperty:@"YES" forKey:@"ADURLProtocol" inRequest:mutableRequest];
+    _connection = [[NSURLConnection alloc] initWithRequest:mutableRequest
+                                                  delegate:self
+                                          startImmediately:YES];
+}
+
+- (void)stopLoading
+{
+    AD_LOG_VERBOSE_F(sLog, nil, @"Stop loading");
+    [_connection cancel];
+    [self.client URLProtocol:self didFailWithError:[NSError errorWithDomain:NSCocoaErrorDomain code:NSUserCancelledError userInfo:nil]];
+}
+
+#pragma mark - NSURLConnectionDelegate Methods
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    (void)connection;
+    
+    AD_LOG_VERBOSE_F(sLog, nil, @"connection:didFaileWithError: %@", error);
+    [self.client URLProtocol:self didFailWithError:error];
+}
+
+-(void) connection:(NSURLConnection *)connection
+willSendRequestForAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge
+{
+    NSString* authMethod = [challenge.protectionSpace.authenticationMethod lowercaseString];
+    
+    AD_LOG_VERBOSE_F(sLog, nil, @"connection:willSendRequestForAuthenticationChallenge: %@. Previous challenge failure count: %ld", authMethod, (long)challenge.previousFailureCount);
+    
+    BOOL handled = NO;
+    @synchronized([self class])
+    {
+        Class<ADAuthMethodHandler> handler = [s_handlers objectForKey:authMethod];
+        handled = [handler handleChallenge:challenge
+                                connection:connection
+                                  protocol:self];
+        
+    }
+    
+    if (!handled)
+    {
+        // Do default handling
+        [challenge.sender performDefaultHandlingForAuthenticationChallenge:challenge];
+    }
+}
+
+#pragma mark - NSURLConnectionDataDelegate Methods
+
+- (NSURLRequest *)connection:(NSURLConnection *)connection willSendRequest:(NSURLRequest *)request redirectResponse:(NSURLResponse *)response
+{
+    (void)connection;
+    
+    AD_LOG_VERBOSE_F(sLog, nil, @"HTTPProtocol::connection:willSendRequest:. Redirect response: %@. New request:%@", response.URL, request.URL);
+    //Ensure that the webview gets the redirect notifications:
+    NSMutableURLRequest* mutableRequest = [request mutableCopy];
+    if (response)
+    {
+        [[self class] removePropertyForKey:@"ADURLProtocol" inRequest:mutableRequest];
+        [self.client URLProtocol:self wasRedirectedToRequest:mutableRequest redirectResponse:response];
+        
+        [_connection cancel];
+        [self.client URLProtocol:self didFailWithError:[NSError errorWithDomain:NSCocoaErrorDomain code:NSUserCancelledError userInfo:nil]];
+        [ADCustomHeaderHandler applyCustomHeadersTo:mutableRequest];
+
+        return mutableRequest;
+    }
+    
+	[ADCustomHeaderHandler applyCustomHeadersTo:mutableRequest];
+    return mutableRequest;
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    (void)connection;
+    
+    [self.client URLProtocol:self didReceiveResponse:response cacheStoragePolicy:NSURLCacheStorageNotAllowed];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    (void)connection;
+    
+    [self.client URLProtocol:self didLoadData:data];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    (void)connection;
+    
+    [self.client URLProtocolDidFinishLoading:self];
+    _connection = nil;
+}
+
+
+@end
