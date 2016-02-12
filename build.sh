e2fa08cc3e91238b95d6e808bd80762ad693b4a3
Ryan Pangrle <rpangrle@microsoft.com>
2016-02-11 17:57:52 -0800

    Even more attempts to get travis happy

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-xcodebuild test -project ADALiOS/ADALiOS.xcodeproj -scheme ADALiOS -configuration CodeCoverage -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest' | xcpretty -c
-xcodebuild -workspace ADALiOS.xcworkspace -scheme MyTestiOSApp -configuration CodeCoverage -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest' | xcpretty -c
+find . -name "*.gcda" -print0 | xargs -0 rm
 
-slather coverage -s ADALiOS/ADALiOS.xcodeproj
+xcodebuild test -workspace ADAL.xcworkspace -scheme ADAL -configuration CodeCoverage -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest' | xcpretty
+xcodebuild build -workspace ADAL.xcworkspace -scheme MyTestiOSApp -configuration CodeCoverage -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest' | xcpretty
+xcodebuild test -workspace ADAL.xcworkspace -scheme "ADAL Mac" | xcpretty
+xcodebuild test -workspace ADAL.xcworkspace -scheme "ADAL Mac" CURRENT_ARCH=i386 | xcpretty
+
+# slather coverage -s ADAL/ADAL.xcodeproj
