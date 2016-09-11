a9ed2b086468c3a4d662aa1f46fb012d3e1b3767
Ryan Pangrle <rpangrle@microsoft.com>
2016-09-11 13:20:46 -0700

    Add check for end URL in the webview delegate
    We do still hit this when the redirect URI is an "http://" URL

--- ADALiOS/ADALiOS/ADAuthenticationWebViewController.m
+++ ADALiOS/ADALiOS/ADAuthenticationWebViewController.m
@@ -215,6 +215,19 @@ NSTimer *timer;
         return;
     }
     
+    // If we failed on an invalid URL check to see if it matches our end URL
+    if ([error.domain isEqualToString:@"NSURLErrorDomain"] && (error.code == -1002 || error.code == -1003))
+    {
+        NSURL* url = [error.userInfo objectForKey:NSURLErrorFailingURLErrorKey];
+        NSString* urlString = [url absoluteString];
+        if ([[urlString lowercaseString] hasPrefix:_endURL.lowercaseString])
+        {
+            _complete = YES;
+            dispatch_async( dispatch_get_main_queue(), ^{ [_delegate webAuthenticationDidCompleteWithURL:url]; } );
+            return;
+        }
+    }
+    
     // Prior to iOS 10 the WebView trapped out this error code and didn't pass it along to us
     // now we have to trap it out ourselves.
     if ([error.domain isEqualToString:NSCocoaErrorDomain] && error.code == NSUserCancelledError)
