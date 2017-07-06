aa17c983743a0e6266851f270bb5ebc9b6b2dd58
Ryan Pangrle <rpangrle@microsoft.com>
2017-07-06 14:44:18 -0700

    Cache the result from acquireTokenByAssertion calls

--- ADAL/src/request/ADAuthenticationRequest+AcquireToken.m
+++ ADAL/src/request/ADAuthenticationRequest+AcquireToken.m
@@ -269,7 +269,17 @@
     
     if (_samlAssertion)
     {
-        [self requestTokenByAssertion:completionBlock];
+        [self requestTokenByAssertion:^(ADAuthenticationResult *result){
+            if (AD_SUCCEEDED == result.status)
+            {
+                [[_requestParams tokenCache] updateCacheToResult:result
+                                                       cacheItem:nil
+                                                    refreshToken:nil
+                                                         context:_requestParams];
+                result = [ADAuthenticationContext updateResult:result toUser:[_requestParams identifier]];
+            }
+            completionBlock(result);
+        }];
         return;
     }
 
