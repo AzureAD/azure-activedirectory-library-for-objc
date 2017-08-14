02ff2559eb1d79ed6763e6ef4a2f87e26887ea14
Sergey Demchenko <sedemche@microsoft.com>
2017-08-14 10:06:24 -0700

    Refactor ADAuthenticationResultTests.

--- ADAL/tests/unit/ADAuthenticationResultTests.m
+++ ADAL/tests/unit/ADAuthenticationResultTests.m
@@ -44,58 +44,58 @@
     [super tearDown];
 }
 
-//Only static creators and internal initializers are supported. init and new should throw.
-- (void) testInitAndNew
+#pragma mark - Initialization
+
+- (void)testNew_shouldThrow
 {
-    XCTAssertThrows([[ADAuthenticationResult alloc] init]);
     XCTAssertThrows([ADAuthenticationResult new]);
 }
 
-#define VERIFY_RESULT(_result, _status, _code) { \
-    XCTAssertNotNil(_result); \
-    XCTAssertEqual(_result.status, _status, "Wrong status"); \
-    XCTAssertNotNil(_result.error, "Nil error"); \
-    ADAssertLongEquals(_result.error.code, _code); \
-    XCTAssertNil(_result.tokenCacheItem.accessToken); \
-    XCTAssertNil(_result.tokenCacheItem.accessTokenType); \
-    XCTAssertNil(_result.tokenCacheItem.refreshToken); \
-    XCTAssertNil(_result.tokenCacheItem.expiresOn); \
-    XCTAssertNil(_result.tokenCacheItem.userInformation); \
+- (void)testInit_shouldThrow
+{
+    XCTAssertThrows([[ADAuthenticationResult alloc] init]);
 }
 
--(void) testResultFromCancellation
-{
-    ADAuthenticationResult* result = [ADAuthenticationResult resultFromCancellation];
-    VERIFY_RESULT(result, AD_USER_CANCELLED, AD_ERROR_UI_USER_CANCEL);
-}
+#pragma mark - resultFromCancellation
 
--(void) testResultFromError
+- (void)testResultFromCancellation_whenNoParameters_shouldReturnCancelledStatusAndErrorTokenCacheItemNil
 {
-    ADAuthenticationError* error = [ADAuthenticationError unexpectedInternalError:@"something" correlationId:nil];
-    ADAuthenticationResult* result = [ADAuthenticationResult resultFromError:error];
-    VERIFY_RESULT(result, AD_FAILED, AD_ERROR_UNEXPECTED);
-    XCTAssertEqualObjects(result.error, error, "Different error object in the result.");
-}
-
--(void) verifyResult: (ADAuthenticationResult*) resultFromItem
-                item: (ADTokenCacheItem*) item
-{
-    XCTAssertNotNil(resultFromItem);
-    XCTAssertEqual(resultFromItem.status, AD_SUCCEEDED, "Result should be success.");
-    XCTAssertNil(resultFromItem.error, "Unexpected error object: %@", resultFromItem.error.errorDetails);
-    XCTAssertEqual(item.accessTokenType, resultFromItem.tokenCacheItem.accessTokenType);
-    XCTAssertEqual(item.accessToken, resultFromItem.tokenCacheItem.accessToken);
-    XCTAssertEqual(item.expiresOn, resultFromItem.tokenCacheItem.expiresOn);
-    XCTAssertEqual(item.userInformation.tenantId, resultFromItem.tokenCacheItem.userInformation.tenantId);
-    ADAssertStringEquals(item.userInformation.userId, resultFromItem.tokenCacheItem.userInformation.userId);
-}
-
-- (void)testResultFromtokenCacheItem
-{
-    ADAuthenticationResult* nilItemResult = [ADAuthenticationResult resultFromTokenCacheItem:nil multiResourceRefreshToken:NO correlationId:nil];
-    VERIFY_RESULT(nilItemResult, AD_FAILED, AD_ERROR_UNEXPECTED);
+    ADAuthenticationResult *result = [ADAuthenticationResult resultFromCancellation];
     
-    ADTokenCacheItem* item = [[ADTokenCacheItem alloc] init];
+    XCTAssertNotNil(result);
+    XCTAssertEqual(result.status, AD_USER_CANCELLED);
+    ADAssertLongEquals(result.error.code, AD_ERROR_UI_USER_CANCEL);
+    XCTAssertNil(result.tokenCacheItem);
+}
+
+#pragma mark - resultFromError
+
+- (void)testResultFromError_whenErrorIsUnexpectedInternal_shouldReturnStatusFailedSameErrorAsWasProvidedTokenChacheItemNil
+{
+    ADAuthenticationError *error = [ADAuthenticationError unexpectedInternalError:@"something" correlationId:nil];
+    ADAuthenticationResult *result = [ADAuthenticationResult resultFromError:error];
+    
+    XCTAssertNotNil(result);
+    XCTAssertEqual(result.status, AD_FAILED);
+    XCTAssertEqualObjects(result.error, error);
+    XCTAssertNil(result.tokenCacheItem);
+}
+
+#pragma mark - resultFromTokenCacheItem
+
+- (void)testResultFromtokenCacheItem_whenItemNilMRRTNoCorrelationIdNil_shouldReturnStatusFailedErrorUnexpectedTokenChacheItemNil
+{
+    ADAuthenticationResult *result = [ADAuthenticationResult resultFromTokenCacheItem:nil multiResourceRefreshToken:NO correlationId:nil];
+
+    XCTAssertNotNil(result);
+    XCTAssertEqual(result.status, AD_FAILED);
+    ADAssertLongEquals(result.error.code, AD_ERROR_UNEXPECTED);
+    XCTAssertNil(result.tokenCacheItem);
+}
+
+- (void)testResultFromtokenCacheItem_whenItemValidMRRTNoCorrelationIdNil_shouldReturnStatusSucceededErrorNilTokenChacheItemSameAsWasProvided
+{
+    ADTokenCacheItem *item = [ADTokenCacheItem new];
     item.resource = @"resource";
     item.authority = @"https://login.windows.net/mytennant.com";
     item.clientId = @"clientId";
@@ -104,17 +104,21 @@
     item.refreshToken = @"refreshToken";
     item.expiresOn = [NSDate dateWithTimeIntervalSinceNow:30];
     
-    //Copy the item to ensure that it is not modified withing the method call below:
-    ADAuthenticationResult* resultFromValidItem = [ADAuthenticationResult resultFromTokenCacheItem:[item copy] multiResourceRefreshToken:NO correlationId:nil];
-    [self verifyResult:resultFromValidItem item:item];
+    ADAuthenticationResult *result = [ADAuthenticationResult resultFromTokenCacheItem:item multiResourceRefreshToken:NO correlationId:nil];
+    
+    XCTAssertNotNil(result);
+    XCTAssertNil(result.error);
+    XCTAssertEqual(result.status, AD_SUCCEEDED);
+    XCTAssertEqualObjects(item, result.tokenCacheItem);
 }
 
-- (void)testBrokerResponse
+#pragma mark - resultFromBrokerResponse
+
+- (void)testResultFromBrokerResponse_whenResponseIsValid_shouldReturnResultWithTokenCacheItem
 {
     // Not a complete IDToken, but enough to get past the parser. If you're seeing this test fail and have recently
     // changed the idtoken code then this might have to be tweaked.
     NSString* idtokenJSON = @"{\"typ\":\"JWT\",\"aud\":\"c3c7f5e5-7153-44d4-90e6-329686d48d76\",\"iss\":\"https://sts.windows.net/6fd1f5cd-a94c-4335-889b-6c598e6d8048/\",\"iat\":1387224169,\"nbf\":1387224169,\"exp\":1387227769,\"ver\":\"1.0\",\"tid\":\"6fd1f5cd-a94c-4335-889b-6c598e6d8048\",\"oid\":\"53c6acf2-2742-4538-918d-e78257ec8516\",\"upn\":\"myfakeuser@contoso.com\",\"unique_name\":\"myfakeuser@contoso.com\",\"sub\":\"0DxnAlLi12IvGL_dG3dDMk3zp6AQHnjgogyim5AWpSc\",\"family_name\":\"User\",\"given_name\":\"Fake\"}";
-    
     NSDictionary* response = @{
                                @"access_token" : @"MyFakeAccessToken",
                                @"refresh_token" : @"MyFakeRefreshToken",
@@ -125,10 +129,12 @@
                                @"id_token" : [idtokenJSON adBase64UrlEncode],
                                @"user_id" : @"myfakeuser@contoso.com"
                                };
-    ADAuthenticationResult* result = [ADAuthenticationResult resultFromBrokerResponse:response];
+    
+    ADAuthenticationResult *result = [ADAuthenticationResult resultFromBrokerResponse:response];
+    
     XCTAssertNotNil(result);
+    XCTAssertNil(result.error);
     XCTAssertNotNil(result.tokenCacheItem);
-    XCTAssertNotNil(result.tokenCacheItem.expiresOn);
     XCTAssertEqual(result.tokenCacheItem.expiresOn.timeIntervalSince1970, 1444166530.336707);
     XCTAssertEqualObjects(result.tokenCacheItem.accessToken, @"MyFakeAccessToken");
     XCTAssertEqualObjects(result.tokenCacheItem.refreshToken, @"MyFakeRefreshToken");
@@ -137,37 +143,37 @@
     XCTAssertEqualObjects(result.tokenCacheItem.userInformation.userId, @"myfakeuser@contoso.com");
 }
 
-- (void)testBrokerOldErrorResponse
+- (void)testResultFromBrokerResponse_whenResponseIsInOldFormat_shouldReturnResultWithParsedValues
 {
     // Older versions of the broker send the protocol code in "code", the error details in "error_details" and
     // nothing else. Let's at least try to use all this info.
-    NSDictionary* response = @{
+    NSDictionary *response = @{
                                @"code" : @"could_not_compute",
                                @"error_description" : @"EXTERMINATE!!!!!!",
                                @"correlation_id" : @"5EF4B8D0-A734-441B-887D-FBB8257C0784"
                                };
     
-    ADAuthenticationResult* result = [ADAuthenticationResult resultFromBrokerResponse:response];
+    ADAuthenticationResult *result = [ADAuthenticationResult resultFromBrokerResponse:response];
     
     XCTAssertNotNil(result);
-    XCTAssertNil(result.tokenCacheItem);
     XCTAssertNotNil(result.error);
+    XCTAssertNil(result.tokenCacheItem);
+    XCTAssertEqualObjects(result.correlationId, [[NSUUID alloc] initWithUUIDString:@"5EF4B8D0-A734-441B-887D-FBB8257C0784"]);
     XCTAssertEqualObjects(result.error.errorDetails, @"EXTERMINATE!!!!!!");
     XCTAssertEqualObjects(result.error.protocolCode, @"could_not_compute");
-    XCTAssertEqualObjects(result.correlationId, [[NSUUID alloc] initWithUUIDString:@"5EF4B8D0-A734-441B-887D-FBB8257C0784"]);
     XCTAssertEqual(result.error.code, AD_ERROR_TOKENBROKER_UNKNOWN);
 }
 
-- (void)testBrokerFullErrorResponse
+- (void)testResultFromBrokerResponse_whenResponseHasFullErrorDetails_shouldReturnResultWithError
 {
-    NSDictionary* response = @{
+    NSDictionary *response = @{
                                @"error_code" : @"5",
                                @"protocol_code" : @"wibbly_wobbly",
                                @"error_description" : @"timey wimey",
                                @"correlation_id" : @"5EF4B8D0-A734-441B-887D-FBB8257C0784"
                                };
 
-    ADAuthenticationResult* result = [ADAuthenticationResult resultFromBrokerResponse:response];
+    ADAuthenticationResult *result = [ADAuthenticationResult resultFromBrokerResponse:response];
     
     XCTAssertNotNil(result);
     XCTAssertNil(result.tokenCacheItem);
@@ -178,9 +184,9 @@
     XCTAssertEqualObjects(result.correlationId, [[NSUUID alloc] initWithUUIDString:@"5EF4B8D0-A734-441B-887D-FBB8257C0784"]);
 }
 
-- (void)testBrokerNonNetworkResponse
+- (void)testResultFromBrokerResponse_whenNotNetworkResponse_shouldReturnResultWithError
 {
-    NSDictionary* response = @{
+    NSDictionary *response = @{
                                @"error_code" : @"6",
                                @"error_description" : @"I can't find my pants.",
                                @"correlation_id" : @"5EF4B8D0-A734-441B-887D-FBB8257C0784"
