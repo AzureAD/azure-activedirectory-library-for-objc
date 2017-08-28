cae876f9002221f14a4a552650f26ac1d7e37675
Ryan Pangrle <rpangrle@microsoft.com>
2017-08-28 12:19:04 -0700

    Add script and .travis.yml to dump any test logs after a build failure into the build log

---
+++ print_logs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for f in ~/Library/Developer/Xcode/DerivedData/*/Logs/Test/*/*; do
+	bname=$(basename $f)
+	echo "travis_fold:start:$bname"
+	cat $f
+	echo "travis_fold:end:$bname"
+done
