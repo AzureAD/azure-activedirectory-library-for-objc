aeb22d13254d288d810e03f6eba8db063f43645b
Yong Zeng <zeyong@microsoft.com>
2017-12-08 15:39:52 -0800

    add ADAuthenticationErrorConverter

---
+++ ADAL/src/ADAuthenticationErrorConverter.m
@@ -0,0 +1,93 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "ADAuthenticationErrorConverter.h"
+#import "ADAuthenticationError.h"
+#import "MSIDError.h"
+
+static NSDictionary* s_errorDomainMapping;
+static NSDictionary* s_errorCodeMapping;
+
+@interface ADAuthenticationError (ErrorConverterUtil)
++ (ADAuthenticationError *)errorWithDomainInternal:(NSString *)domain
+                                              code:(NSInteger)code
+                                 protocolErrorCode:(NSString *)protocolCode
+                                      errorDetails:(NSString *)details
+                                     correlationId:(NSUUID *)correlationId
+                                          userInfo:(NSDictionary *)userInfo;
+@end
+
+@implementation ADAuthenticationErrorConverter
+
++ (void)initialize
+{
+    s_errorDomainMapping = @{
+                             MSIDErrorDomain : ADAuthenticationErrorDomain
+                             };
+    
+    s_errorCodeMapping = @{
+                           //sample format is like @"MSIDErrorDomain|-10000":@"-20000"
+                           };
+}
+
++ (ADAuthenticationError *)ADAuthenticationErrorFromMSIDError:(NSError *)msidError
+                                                correlationId:(NSUUID *)correlationId
+{
+    if (!msidError)
+    {
+        return nil;
+    }
+    
+    //Map domain
+    NSString *domain = msidError.domain;
+    if (domain && s_errorDomainMapping[domain])
+    {
+        domain = s_errorDomainMapping[domain];
+    }
+    
+    //Map errorCode. Note that errorCode must be mapped together with domain
+    NSInteger errorCode = msidError.code;
+    NSString *mapKey = [NSString stringWithFormat:@"%@|%ld", msidError.domain, (long)errorCode];
+    NSString *mapValue = s_errorCodeMapping[mapKey];
+    if (![NSString msidIsStringNilOrBlank:mapValue])
+    {
+        errorCode = [mapValue integerValue];
+    }
+    
+    NSMutableDictionary *userInfo = nil;
+    if (msidError.userInfo[NSUnderlyingErrorKey] || msidError.userInfo[MSIDHTTPHeadersKey])
+    {
+        userInfo = [NSMutableDictionary new];
+        [userInfo setValue:msidError.userInfo[NSUnderlyingErrorKey] forKey:NSUnderlyingErrorKey];
+        [userInfo setValue:msidError.userInfo[MSIDHTTPHeadersKey] forKey:ADHTTPHeadersKey];
+    }
+    
+    return [ADAuthenticationError errorWithDomainInternal:domain
+                                                     code:errorCode
+                                        protocolErrorCode:msidError.userInfo[MSIDOAuthErrorKey]
+                                             errorDetails:msidError.userInfo[MSIDErrorDescriptionKey]
+                                            correlationId:correlationId
+                                                 userInfo:userInfo];
+}
+
+@end
---
+++ ADAL/tests/unit/ADAuthenticationErrorConverterTests.m
@@ -0,0 +1,75 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import <XCTest/XCTest.h>
+#import "ADAuthenticationError.h"
+#import "ADAuthenticationErrorConverter.h"
+#import "MSIDError.h"
+
+@interface ADAuthenticationErrorConverterTests : XCTestCase
+
+@end
+
+@implementation ADAuthenticationErrorConverterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testErrorConversion_whenPassInNil_ShouldReturnNil {
+    NSError *adalError = [ADAuthenticationErrorConverter ADAuthenticationErrorFromMSIDError:nil correlationId:nil];
+    XCTAssertNil(adalError);
+}
+
+- (void)testErrorConversion_whenOnlyErrorDomainIsMapped_ErrorCodeShouldBeKept {
+    NSInteger errorCode = -9999;
+    NSString *errorDescription = @"a fake error description.";
+    NSString *oauthError = @"a fake oauth error message.";
+    NSError *underlyingError = [NSError errorWithDomain:NSOSStatusErrorDomain code:errSecItemNotFound userInfo:nil];
+    NSDictionary *httpHeaders = @{@"fake header key" : @"fake header value"};
+    
+    NSError *msidError = MSIDCreateError(MSIDErrorDomain,
+                                         errorCode,
+                                         errorDescription,
+                                         oauthError,
+                                         nil,
+                                         underlyingError,
+                                         httpHeaders);
+    ADAuthenticationError *adalError = [ADAuthenticationErrorConverter ADAuthenticationErrorFromMSIDError:msidError correlationId:nil];
+    
+    XCTAssertNotNil(adalError);
+    XCTAssertEqualObjects(adalError.domain, ADAuthenticationErrorDomain);
+    XCTAssertEqual(adalError.code, errorCode);
+    XCTAssertEqualObjects(adalError.errorDetails, errorDescription);
+    XCTAssertEqualObjects(adalError.protocolCode, oauthError);
+    XCTAssertEqualObjects(adalError.userInfo[NSUnderlyingErrorKey], underlyingError);
+    XCTAssertEqualObjects(adalError.userInfo[ADHTTPHeadersKey], httpHeaders);
+}
+
+@end
