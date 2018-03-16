6a8b53aef6cef03c9d43755e1906f1b163e7e61a
Olga Dalton <oldalton@microsoft.com>
2018-03-15 18:22:53 -0700

    Updated error codes

--- ADAL/src/ADAuthenticationErrorConverter.m
+++ ADAL/src/ADAuthenticationErrorConverter.m
@@ -54,7 +54,13 @@ static NSDictionary *s_userInfoKeyMapping;
                                    @(MSIDErrorInteractionRequired):@(AD_ERROR_SERVER_USER_INPUT_NEEDED),
                                    @(MSIDErrorCacheMultipleUsers):@(AD_ERROR_CACHE_MULTIPLE_USERS),
                                    @(MSIDErrorTokenCacheItemFailure):@(AD_ERROR_CACHE_BAD_FORMAT),
+                                   @(MSIDErrorCacheBadFormat): @(AD_ERROR_CACHE_BAD_FORMAT),
                                    @(MSIDErrorServerInvalidResponse):@(AD_ERROR_SERVER_INVALID_RESPONSE),
+                                   @(MSIDErrorServerRefreshTokenRejected):@(AD_ERROR_SERVER_REFRESH_TOKEN_REJECTED),
+                                   @(MSIDErrorServerOauth): @(AD_ERROR_SERVER_OAUTH),
+                                   @(MSIDErrorInvalidRequest): @(AD_ERROR_SERVER_OAUTH),
+                                   @(MSIDErrorInvalidClient): @(AD_ERROR_SERVER_OAUTH),
+                                   @(MSIDErrorInvalidGrant): @(AD_ERROR_SERVER_REFRESH_TOKEN_REJECTED),
                                    @(MSIDErrorDeveloperAuthorityValidation):@(AD_ERROR_DEVELOPER_AUTHORITY_VALIDATION)
                                    }
                            };
