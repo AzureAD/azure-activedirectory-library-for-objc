5345fed351e5e6d2791bd5fa1a410c2a9fc1438a
Mike Della Donna <midella@microsoft.com>
2018-06-11 14:55:25 -0400

    Add ADEnrollmentGateway

---
+++ ADAL/src/ADEnrollmentGateway.m
@@ -0,0 +1,158 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "ADEnrollmentGateway.h"
+
+// Keys for Intune Enrollment ID
+NSString* const ADIntuneAppProtectionEnrollmentID = @"intune_app_protection_enrollment_id_V";
+NSString* const ADIntuneAppProtectionEnrollmentIDVersion = @"1";
+#define AD_INTUNE_ENROLLMENT_ID_KEY [ADIntuneAppProtectionEnrollmentID stringByAppendingString:ADIntuneAppProtectionEnrollmentIDVersion]
+NSString* const enrollmentIdArray = @"enrollment_ids";
+
+NSString* const tid = @"tid";
+NSString* const oid = @"oid";
+NSString* const unique_account_id = @"unique_account_id";
+NSString* const user_id = @"user_id";
+NSString* const enrollment_id = @"enrollment_id";
+
+// Keys for Intune Resource
+NSString* const ADIntuneAppProtectionResourceID = @"intune_mam_resource_V";
+NSString* const ADIntuneAppProtectionResourceIDVersion = @"1";
+#define AD_INTUNE_RESOURCE_ID_KEY [ADIntuneAppProtectionEnrollmentID stringByAppendingString:ADIntuneAppProtectionEnrollmentIDVersion]
+
+static NSString* ADIntuneEnrollmentIdJSON = nil;
+static NSString* ADIntuneResourceJSON = nil;
+
+
+@interface ADEnrollmentGateway()
+
++ (NSString*) getEnrollmentIDForIdentifier:(BOOL (^)(NSDictionary*)) idBlock;
++ (NSString *) normalizeAuthority:(NSString *)authority;
+
+@end
+
+@implementation ADEnrollmentGateway
+
++ (NSString*) getEnrollmentIDForIdentifier:(BOOL (^)(NSDictionary*)) idBlock
+{
+    NSString* enrollIdJSON = [ADEnrollmentGateway allEnrollmentIds];
+
+    NSError* error = nil;
+    id enrollIds = [NSJSONSerialization JSONObjectWithData:[enrollIdJSON dataUsingEncoding:NSUTF8StringEncoding] options:kNilOptions error:&error];
+
+    if (error)
+        return nil;
+
+    enrollIds = enrollIds[enrollmentIdArray];
+
+    if (!enrollIds)
+        return nil;
+
+    for (NSDictionary* enrollIdDic in enrollIds)
+    {
+        if (idBlock(enrollIdDic))
+            return [enrollIdDic objectForKey:enrollment_id];
+    }
+
+    return nil;
+}
+
++ (NSString *)allEnrollmentIds
+{
+    if (ADIntuneEnrollmentIdJSON)
+        return ADIntuneEnrollmentIdJSON;
+
+    return [[NSUserDefaults standardUserDefaults] objectForKey:AD_INTUNE_ENROLLMENT_ID_KEY];
+}
+
++ (NSString *)enrollmentIdForUserId:(NSString *)userId;
+{
+    return [ADEnrollmentGateway getEnrollmentIDForIdentifier:^BOOL(NSDictionary * dic) {
+        return [[dic objectForKey:user_id] isEqualToString:userId];
+    }];
+}
+
++ (NSString *)enrollmentIdForUserObjectId:(NSString *)userObjectId tenantId:(NSString *)tenantId
+{
+    return [ADEnrollmentGateway getEnrollmentIDForIdentifier:^BOOL(NSDictionary * dic) {
+        return [[dic objectForKey:oid] isEqualToString:userObjectId] && [[dic objectForKey:tid] isEqualToString:tenantId];
+    }];
+}
+
++ (NSString *)enrollmentIdForUniqueAccountId:(NSString *)uniqueAccountId
+{
+    return [ADEnrollmentGateway getEnrollmentIDForIdentifier:^BOOL(NSDictionary * dic) {
+        return [[dic objectForKey:unique_account_id] isEqualToString:uniqueAccountId];
+    }];
+}
+
++ (NSString *) normalizeAuthority:(NSString *)authority
+{
+    NSURL* authorityURL = [NSURL URLWithString:authority];
+    NSNumber *port = authorityURL.port;
+
+    // This assumes we're using https, which is mandatory for all AAD communications.
+    if (port == nil || port.intValue == 443)
+    {
+        return authorityURL.host.lowercaseString;
+    }
+    return [NSString stringWithFormat:@"%@:%d", authorityURL.host.lowercaseString, port.intValue];
+}
+
++ (NSString *)intuneMamResource:(NSString *)authority
+{
+    NSString* resourceJSON;
+
+    if (ADIntuneResourceJSON)
+    {
+        resourceJSON = ADIntuneResourceJSON;
+    }
+    else
+    {
+        resourceJSON = [[NSUserDefaults standardUserDefaults] objectForKey:AD_INTUNE_RESOURCE_ID_KEY];
+        if (!resourceJSON)
+            return nil;
+    }
+
+    NSError* error = nil;
+    id resources = [NSJSONSerialization JSONObjectWithData:[resourceJSON dataUsingEncoding:NSUTF8StringEncoding] options:kNilOptions error:&error];
+
+    if (error)
+        return nil;
+
+    return resources[[ADEnrollmentGateway normalizeAuthority:authority]];
+}
+
+#if AD_BROKER
++ (void)setIntuneMamResourceWithJsonBlob:(NSString *)resources
+{
+    ADIntuneResourceJSON = [resources copy];
+}
+
++ (void)setEnrollmentIdsWithJsonBlob:(NSString *)enrollmentIds
+{
+    ADIntuneEnrollmentIdJSON = [enrollmentIds copy];
+}
+#endif
+
+@end
---
+++ ADAL/tests/unit/ios/ADEnrollmentGatewayTests.m
@@ -0,0 +1,109 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import <XCTest/XCTest.h>
+#import "XCTestCase+TestHelperMethods.h"
+#import "ADEnrollmentGateway.h"
+#import <objc/runtime.h>
+
+@interface ADEnrollmentGatewayTests : ADTestCase
+
+@property NSString* testJSON;
+@property IMP originalAllEnrollmentIds;
+
+@end
+
+@implementation ADEnrollmentGatewayTests
+
+- (void)setUp
+{
+    [super setUp];
+
+    self.testJSON = [NSString stringWithFormat:
+        @"{\"enrollment_ids\": [\n"
+                                    "{\n"
+                                        "\"tid\" : \"fda5d5d9-17c3-4c29-9cf9-a27c3d3f03e1\",\n"
+                                        "\"oid\" : \"d3444455-mike-4271-b6ea-e499cc0cab46\",\n"
+                                        "\"unique_account_id\" : \"60406d5d-mike-41e1-aa70-e97501076a22\",\n"
+                                        "\"user_id\" : \"mike@contoso.com\",\n"
+                                        "\"enrollment_id\" : \"adf79e3f-mike-454d-9f0f-2299e76dbfd5\"\n"
+                                    "},\n"
+                                     "{\n"
+                                         "\"tid\" : \"fda5d5d9-17c3-4c29-9cf9-a27c3d3f03e1\",\n"
+                                         "\"oid\" : \"6eec576f-dave-416a-9c4a-536b178a194a\",\n"
+                                         "\"unique_account_id\" : \"1e4dd613-dave-4527-b50a-97aca38b57ba\",\n"
+                                         "\"user_id\" : \"dave@contoso.com\",\n"
+                                         "\"enrollment_id\" : \"64d0557f-dave-4193-b630-8491ffd3b180\"\n"
+                                     "},\n"
+                                    "]\n"
+            "}"];
+
+    // mock out enrollment data
+
+    Class ADEnrollmentGatewayClass = [ADEnrollmentGateway class];
+    SEL allEnrollmentIDsSelector = @selector(allEnrollmentIds);
+    Method allEnrollmentIDsMethod = class_getClassMethod(ADEnrollmentGatewayClass, allEnrollmentIDsSelector);
+    self.originalAllEnrollmentIds = method_getImplementation(allEnrollmentIDsMethod);
+
+    method_setImplementation(allEnrollmentIDsMethod, imp_implementationWithBlock(^NSString*(){return self.testJSON;}));
+
+
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+
+    // restore mocked method
+
+    Class ADEnrollmentGatewayClass = [ADEnrollmentGateway class];
+    SEL allEnrollmentIDsSelector = @selector(allEnrollmentIds);
+    Method allEnrollmentIDsMethod = class_getClassMethod(ADEnrollmentGatewayClass, allEnrollmentIDsSelector);
+
+    method_setImplementation(allEnrollmentIDsMethod, self.originalAllEnrollmentIds);
+}
+
+- (void) testenrollmentIdForUserId
+{
+    XCTAssert([@"adf79e3f-mike-454d-9f0f-2299e76dbfd5" isEqualToString:[ADEnrollmentGateway enrollmentIdForUserId:@"mike@contoso.com"]]);
+
+    XCTAssert([@"64d0557f-dave-4193-b630-8491ffd3b180" isEqualToString:[ADEnrollmentGateway enrollmentIdForUserId:@"dave@contoso.com"]]);
+}
+
+- (void) testenrollmentIdForUserObjectIdtenantId
+{
+    XCTAssert([@"adf79e3f-mike-454d-9f0f-2299e76dbfd5" isEqualToString: [ADEnrollmentGateway enrollmentIdForUserObjectId:@"d3444455-mike-4271-b6ea-e499cc0cab46" tenantId:@"fda5d5d9-17c3-4c29-9cf9-a27c3d3f03e1"]]);
+
+    XCTAssert([@"64d0557f-dave-4193-b630-8491ffd3b180" isEqualToString: [ADEnrollmentGateway enrollmentIdForUserObjectId:@"6eec576f-dave-416a-9c4a-536b178a194a" tenantId:@"fda5d5d9-17c3-4c29-9cf9-a27c3d3f03e1"]]);
+}
+
+- (void) testenrollmentIdForUniqueAccountId
+{
+    XCTAssert([@"adf79e3f-mike-454d-9f0f-2299e76dbfd5" isEqualToString: [ADEnrollmentGateway enrollmentIdForUniqueAccountId:@"60406d5d-mike-41e1-aa70-e97501076a22"]]);
+
+    XCTAssert([@"64d0557f-dave-4193-b630-8491ffd3b180" isEqualToString: [ADEnrollmentGateway enrollmentIdForUniqueAccountId:@"1e4dd613-dave-4527-b50a-97aca38b57ba"]]);
+}
+
+
+
+@end
