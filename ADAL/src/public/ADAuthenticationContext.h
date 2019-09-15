9e2716f3c612ce4df62d0f2a14b145df70a51219
Olga Dalton <oldalton@microsoft.com>
2019-09-14 22:44:04 -0700

    Fixed macOS build

--- ADAL/src/public/ADAuthenticationContext.h
+++ ADAL/src/public/ADAuthenticationContext.h
@@ -231,7 +231,6 @@ typedef enum
                                                       validateAuthority:(BOOL)validate
                                                             sharedGroup:(nullable NSString*)sharedGroup
                                                                   error:(ADAuthenticationError* __autoreleasing _Nullable * _Nullable)error;
-#endif
 
 /*!
  Ask ADAL to handle URL response. You MUST implement this API in following cases:
@@ -254,6 +253,8 @@ typedef enum
             NO if it is not ADAL response or there was a failure in handling.
  */
 + (BOOL)handleADALResponse:(nonnull NSURL *)response sourceApplication:(nullable NSString *)sourceApplication;
+
+#endif
 
 /*! Represents the authority used by the context. */
 @property (readonly, nonnull) NSString* authority;
