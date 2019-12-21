9694aff2748f7e65d8fbe9f63ecaf1bf0d7392a0
Olga Dalton <oldalton@microsoft.com>
2019-12-21 15:26:03 -0800

    Fixed parent controller logic

--- ADAL/tests/app/src/ios/ADTestAppAcquireTokenViewController.m
+++ ADAL/tests/app/src/ios/ADTestAppAcquireTokenViewController.m
@@ -644,6 +644,7 @@
                                                                         validateAuthority:validateAuthority
                                                                                     error:&error];
     context.clientCapabilities = capabilities;
+    context.parentController = self;
 
     if (!context)
     {
