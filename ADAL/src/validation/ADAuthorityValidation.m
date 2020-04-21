14843798f94a37b223979a099d1f20fd7d58926f
Olga Dalton <oldalton@microsoft.com>
2020-04-20 17:40:50 -0700

    Fixed authority aliases for custom authorities

--- ADAL/src/validation/ADAuthorityValidation.m
+++ ADAL/src/validation/ADAuthorityValidation.m
@@ -185,6 +185,7 @@ static NSString* const s_kWebFingerError               = @"WebFinger request was
     {
         // Validate AAD authority
         [self validateAADAuthority:authorityURL
+                 validateAuthority:validateAuthority
                      requestParams:requestParams
                    completionBlock:^(BOOL validated, ADAuthenticationError *error)
          {
@@ -203,6 +204,7 @@ static NSString* const s_kWebFingerError               = @"WebFinger request was
 // If the authority is known, the server will set the "tenant_discovery_endpoint" parameter in the response.
 // The method should be executed on a thread that is guarranteed to exist upon completion, e.g. the UI thread.
 - (void)validateAADAuthority:(NSURL *)authority
+           validateAuthority:(BOOL)validateAuthority
                requestParams:(ADRequestParameters *)requestParams
              completionBlock:(ADAuthorityValidationCallback)completionBlock
 {
@@ -224,6 +226,7 @@ static NSString* const s_kWebFingerError               = @"WebFinger request was
         __block dispatch_semaphore_t dsem = dispatch_semaphore_create(0);
         
         [self requestAADValidation:authority
+                 validateAuthority:validateAuthority
                      requestParams:requestParams
                    completionBlock:^(BOOL validated, ADAuthenticationError *error)
          {
@@ -254,6 +257,7 @@ static NSString* const s_kWebFingerError               = @"WebFinger request was
 }
 
 - (void)requestAADValidation:(NSURL *)authorityUrl
+           validateAuthority:(BOOL)validateAuthority
                requestParams:(ADRequestParameters *)requestParams
              completionBlock:(ADAuthorityValidationCallback)completionBlock
 {
@@ -277,7 +281,7 @@ static NSString* const s_kWebFingerError               = @"WebFinger request was
     
     NSString *trustedHost = ADTrustedAuthorityWorldWide;
     
-    if ([ADAuthorityUtils isKnownHost:authority.url])
+    if ([ADAuthorityUtils isKnownHost:authority.url] || !validateAuthority)
     {
         trustedHost = authority.environment;
     }
