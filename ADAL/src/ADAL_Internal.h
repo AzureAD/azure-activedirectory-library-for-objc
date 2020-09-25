f64b4bcdf39ee2284a116ac1b373fea2abdaafdc
Peter Lee <peterlee@MININT-HLP4R7A.redmond.corp.microsoft.com>
2020-09-24 20:05:43 -0700

    ..

--- ADAL/src/ADAL_Internal.h
+++ ADAL/src/ADAL_Internal.h
@@ -28,7 +28,7 @@
 
 #define ADAL_VER_HIGH       4
 #define ADAL_VER_LOW        0
-#define ADAL_VER_PATCH      9
+#define ADAL_VER_PATCH      10
 
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
